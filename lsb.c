15ebc9b831306a9fcdc4b7b693bcf3ae63e2d794
Matthew DiBello <matthew.quickbeam@gmail.com>
2018-01-28 17:53:26 -0500

    A little more work

--- lsb.c
+++ lsb.c
@@ -7,12 +7,29 @@ int masks[8] = {1, 2, 4, 8, 16, 32, 48, 64};
 
 int main(int argc, char* argv[]) {
 
-    FILE* img_ptr = fopen("connor.png", "w+");
+    FILE* orig = fopen("connor.png", "w+");
+    char c;
+    int i;
 
-    // iterate through file
+    c = getc(orig);
+    while (c != EOF) {
 
-    // print each bit
+        printf("%c\n", c);
 
+        for (i = 7; i >= 0; i--) {
+
+            printf("Hello?\n");
+
+            if (c & masks[i] == 1)
+                printf("1");
+            else
+                printf("0");
+
+        }
+
+        c = getc(orig);
+
+    }
 
     return 0;
 }
