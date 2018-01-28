a49ceb6d2141833888121d4be8e67715c4d84711
Matthew DiBello <matthew.quickbeam@gmail.com>
2018-01-28 13:37:55 -0500

    Initial commit

---
+++ lsb.c
@@ -0,0 +1,18 @@
+// A simple application for LSB steganography
+// Matthew DiBello
+
+#include<stdio.h>
+
+int masks[8] = {1, 2, 4, 8, 16, 32, 48, 64};
+
+int main(int argc, char* argv[]) {
+
+    FILE* img_ptr = fopen("connor.png", "w+");
+
+    // iterate through file
+
+    // print each bit
+
+
+    return 0;
+}
